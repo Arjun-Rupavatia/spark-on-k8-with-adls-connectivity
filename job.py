36c93f5f183a22038ed004e8042695b42bcd0493
Arjun-Rupavatia <36376458+Arjun-Rupavatia@users.noreply.github.com>
2021-09-24 11:18:35 +0530

    Update job.py

--- job.py
+++ job.py
@@ -1,11 +1,11 @@
 from pyspark.sql import SparkSession
 from pyspark import SparkContext
 #sc = SparkContext("local", "First App")
-#sc.hadoopConfiguration.set("fs.azure.account.key.cieanalyticsstoragedev.dfs.core.windows.net", "fI59Mavwgl5+sms7p25JI7F1AeO1ZNifUiBee6g/ek0LVwqCUYqcdnMlw01IEQOO2UIHWVQWIsTflbfP0h8exw==")
-#sc._jsc.hadoopConfiguration().set("fs.azure.account.key.cieanalyticsstoragedev.dfs.core.windows.net","fI59Mavwgl5+sms7p25JI7F1AeO1ZNifUiBee6g/ek0LVwqCUYqcdnMlw01IEQOO2UIHWVQWIsTflbfP0h8exw==")
+#sc.hadoopConfiguration.set("fs.azure.account.key.<account>.dfs.core.windows.net", "<key>")
+#sc._jsc.hadoopConfiguration().set("fs.azure.account.key.<account>.dfs.core.windows.net","<key>")
 spark = SparkSession.builder.getOrCreate()
-df = spark.read.text("abfs://spark-analytics-cie-dev-2020-10-26t21-36-45-237z@cieanalyticsstoragedev.dfs.core.windows.net/CIETest/Aman/kafkatest1/azure_cost_tracking_2/record85.txt")
-#df = spark.read.text("hdfs:///CIETest/Aman/kafkatest1/azure_cost_tracking_2/record85.txt")
+df = spark.read.text("abfs://path/record85.txt")
+#df = spark.read.text("hdfs://hdfs_path/record85.txt")
 #df.show()
 #df = spark.read.text("hdfs://localhost:9000/record85.txt")   this is working
 df.show()
